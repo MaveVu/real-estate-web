421efdf410c390410cc65a69dd61b98807a8d59b
Minh Vu <81089974+MaveVu@users.noreply.github.com>
2024-11-26 21:02:33 +1100

    Update median price graph

--- pages/suburb.py
+++ pages/suburb.py
@@ -2,6 +2,7 @@ import streamlit as st
 import pandas as pd
 import altair as alt
 import random
+from utilities import get_ranking, visualize_score, visualize_median, label_hist_pred
 
 @st.cache_data
 def load(file):
@@ -13,11 +14,14 @@ def main():
     
     # Load data
     file = 'data/hist_and_pred_median.csv'
-    file_2 = 'data/suburb_ranking.csv'
+    file_2 = 'data/suburb_score.csv'
 
     data = load(file)
-    ranking = load(file_2)
-    ranking.rename(columns={'SA2_Name': 'suburb', 'total_score': 'score'}, inplace=True)
+    scores = load(file_2)
+
+    ranking = get_ranking(scores)
+    label_hist_pred(data)
+    ranking.rename(columns={'total_score': 'score'}, inplace=True)
     
     
     TOTAL = ranking.shape[0]
@@ -35,41 +39,11 @@ def main():
 
     # Check if a suburb is selected
     if selected_suburb:
-        # Filter the data for the selected suburb
-        selected_row = ranking[ranking['suburb'] == selected_suburb].iloc[0]
-        metrics = ['livability', 'affordability', 'score']
-        values = [selected_row[metric] for metric in metrics]
+        st.write(f'Historical and Predicted Median Prices in {selected_suburb}')
+        visualize_median(data, selected_suburb)
+        st.write(f'Score for {selected_suburb}')
+        visualize_score(data, ranking, selected_suburb)
         
-        # Prepare data for Altair
-        altair_data = pd.DataFrame({
-            'Metric': metrics,
-            'Value': values,
-            'Color': ['blue', 'green', 'orange']
-        })
-
-
-
-        bars = (
-            alt.Chart(altair_data)
-            .mark_bar()
-            .encode(
-                x=alt.X('Metric', title=None, axis=alt.Axis(labelAngle=0)),
-                y=alt.Y('Value', title='Score', scale=alt.Scale(domain=[0, max(values) * 1.1])),
-                color=alt.Color('Color', scale=None),  # Use the 'Color' column for individual bar colors
-                tooltip=['Metric', 'Value']
-            )
-        )
-
-        chart = bars.properties(
-            title=f"Scores for {selected_suburb}",
-            width=600,
-            height=400
-        )
-        
-        # Display the chart in Streamlit
-        st.altair_chart(chart, use_container_width=True)
-
-        st.markdown(f"<h2 style='color: white;'>Score Rank: {selected_row.name + 1} / {TOTAL}</h2>", unsafe_allow_html=True)
 # Run
 if __name__ == '__main__':
     main()
--- utilities.py
+++ utilities.py
@@ -1,5 +1,9 @@
 import pandas as pd
+import streamlit as st
+import altair as alt
+import matplotlib.pyplot as plt
 
+# Address page
 def get_info(address, df):
     cols = ['type', 'beds', 'baths', 'parking', 'num_schools', 'cost']
     column_mapping = {
@@ -38,3 +42,80 @@ def get_coord(address, df):
     coords = df[df['address'] == address]['geometry']
     coords = coords.iloc[0][7:-1].split()
     return f"https://www.google.com/maps?q={coords[1]},{coords[0]}"
+
+
+# Suburb page
+def get_ranking(df):
+    df = df.sort_values('total_score',ascending=False)
+    df = df.reset_index(drop=True)
+    return df
+
+def visualize_score(data, ranking, selected_suburb):
+    TOTAL = ranking.shape[0]
+    # Filter the data for the selected suburb
+    selected_row = ranking[ranking['suburb'] == selected_suburb].iloc[0]
+    metrics = ['livability', 'affordability', 'score']
+    values = [selected_row[metric] for metric in metrics]
+    
+    # Prepare data for Altair
+    altair_data = pd.DataFrame({
+        'Metric': metrics,
+        'Value': values,
+        'Color': ['blue', 'green', 'orange']
+    })
+
+    bars = (
+        alt.Chart(altair_data)
+        .mark_bar()
+        .encode(
+            x=alt.X('Metric', title=None, axis=alt.Axis(labelAngle=0)),
+            y=alt.Y('Value', title='Score', scale=alt.Scale(domain=[0, max(values) * 1.1])),
+            color=alt.Color('Color', scale=None),  # Use the 'Color' column for individual bar colors
+            tooltip=['Metric', 'Value']
+        )
+    )
+
+    chart = bars.properties(
+        width=600,
+        height=400
+    )
+    
+    # Display the chart in Streamlit
+    st.altair_chart(chart, use_container_width=True)
+    st.markdown(f"<h2 style='color: white;'>Score Rank: {selected_row.name + 1} / {TOTAL}</h2>", unsafe_allow_html=True)
+
+def label_hist_pred(data):
+    data['label'] = 'predicted'
+    data.loc[data['year'] <= 2024, 'label'] = 'historical'
+
+def visualize_median(data, selected_suburb):
+    data = data[data['suburb'] == selected_suburb]
+    historical_data = data[data['label'] == 'historical']
+    predicted_data = pd.concat([data[data['year'] == 2024], data[data['label'] == 'predicted']])
+    historical_chart = alt.Chart(historical_data).mark_line(
+        color='blue',
+        strokeWidth=6  # Solid line for historical data
+    ).encode(
+        x='year:O',  # Ordinal axis for year
+        y=alt.Y('median_rental_price:Q', title='price'), # Quantitative axis for median rental price
+        tooltip=['year', 'median_rental_price', 'suburb']
+    ).properties(
+        title='Median Rental Prices: Historical vs Predicted'
+    )
+
+    # Create a line chart for predicted data
+    predicted_chart = alt.Chart(predicted_data).mark_line(
+        color='red',
+        strokeDash=[10, 5],
+        strokeWidth=6  # Dashed line for predicted data
+    ).encode(
+        x='year:O',
+        y=alt.Y('median_rental_price:Q', title='price'),
+        tooltip=['year', 'median_rental_price', 'suburb']
+    )
+
+    # Step 3: Combine the two charts
+    chart = historical_chart + predicted_chart
+
+    # Step 4: Display the Altair chart in Streamlit
+    st.altair_chart(chart, use_container_width=True)
